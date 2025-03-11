15bd088a1f1ca8e66e829920423af3147c8672f9
nichrem <nremack@purdue.edu>
2025-03-10 19:10:17 -0500

    Tweaked fronted design

---
+++ doc-vault/components/extensionNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const extensionNav = () => {
+    return (
+        <nav className="text-white bg-red py-4 px-2">
+            <div className="text-3xl font-semibold">
+                docVault
+            </div>
+        </nav>
+    )
+}
+
+export default extensionNav;
--- doc-vault/extension_src/app/(home)/popup/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const popup = () => {
-    return (
-        <div>hey there is a document</div>
-    )
-}
-
-export default popup
