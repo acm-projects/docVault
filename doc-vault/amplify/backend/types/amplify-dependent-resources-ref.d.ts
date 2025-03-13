7bcbb20fd9346dd91add1c4401a3ac550997a155
noahflores21 <noah_e_flores@icloud.com>
2025-03-12 21:14:27 -0500

    integrated backend + amplify setup with frontend

---
+++ doc-vault/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "docvault": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "FileMetadataTable": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    },
+    "s3docvaultfiles": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
