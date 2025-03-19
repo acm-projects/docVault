72e1322c8bc8fa7728b2798ee5ba889466bd989f
Aima <aimasalman404@gmail.com>
2025-03-19 18:40:11 -0500

    added school page due date tracker

--- doc-vault/src/app/(user)/school/page.tsx
+++ doc-vault/src/app/(user)/school/page.tsx
@@ -1,19 +1,66 @@
-import React from 'react'
+"use client"
+
+import { DueDateTable } from '@/components/DueDateTable'
+import React, { useState } from 'react'
 
 const School = () => {
+  const [files, setFiles] = useState([
+    {
+        name: "Homework 1 - Basic Time Complexity",
+        tag: "Priority",
+        modified: "01/10/2025",
+        due: "01/12/2025",
+    },
+    {
+        name: "Homework 2 - Advanced Time Complexity",
+        modified: "01/20/2025",
+        due: "01/30/2025",
+    },
+    {
+        name: "Quiz 1 - Basic Time Complexity",
+        tag: "Coming Up",
+        modified: "02/10/2025",
+        due: "02/12/2025",
+    },
+    {
+        name: "Homework 3 - Data Structures",
+        modified: "02/10/2025",
+        due: "03/12/2025",
+    },
+    {
+        name: "Reading - Data Structures",
+        modified: "02/10/2025",
+        due: "04/12/2025",
+    },
+    {
+        name: "Exam (Chapters 1-5)",
+        modified: "02/10/2025",
+        due: "04/12/2025",
+    },
+  ])
+
+  const addNewFile = (newFile: any) => {
+    setFiles((prevFiles) => [...prevFiles, newFile])
+  }
+
   return (
     <section id="school">
-      <div className="h-screen snap-start min-h-screen text-darkblue max-container padding-container mt-8">
+      <div className="min-h-screen text-darkblue max-container padding-container mt-8">
         <div className="p-4">
-          <h1 className="text-4xl font-bold text-lighterred">School</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-            deserunt mollit anim id est laborum.
-          </p>
-          </div>
+          <h1 className="py-5 text-4xl font-bold text-lighterred">School</h1>
+        </div>
+
+        <div className="p-4 py-5">
+          <DueDateTable files={files} addNewFile={addNewFile} />
+        </div>
+
+        <div className="p-4 py-5">
+          <h1 className="py-5 text-2xl font-bold text-lighterred">Notes</h1>
+        </div>
+
+        <div className="p-4 py-5">
+          <h1 className="py-5 text-2xl font-bold text-lighterred">Syllabus</h1>
+        </div>
       </div>
     </section>
   )
